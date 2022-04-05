d397c3dec45197536d843345185975018a6b582a
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-04-05 18:03:34 +0200

    WIP: SPARQL integration

--- src/api.py
+++ src/api.py
@@ -1,8 +1,9 @@
 import json
 import os
 from flask import Flask, Response, request
+from rdflib.term import Variable, URIRef, Literal
 from sariIiifClipSearch import Query
-
+from sariSparqlParser import parser
 
 try:
   dataDir = os.environ['CLIP_DATA_DIRECTORY']
@@ -35,7 +36,90 @@ def query():
         result = queryWithString(queryString, minScore=minScore, numResults=limit)
         return Response(json.dumps(result), mimetype='application/json')
     return Response('{"status": "OK"}', mimetype='application/json')
+
+@app.route('/sparql', methods=['GET', 'POST'])
+def sparql():
+    if 'query' in request.values:
+        query = request.values['query']
+        response = processSparqlQuery(query)
+        return Response(json.dumps(response), mimetype='application/json')
     
+    return Response('{"status": "OK"}', mimetype='application/json')
+
+def extractRequestFromSparqlQuery(query):
+    """
+    Given an SPARQL query this function extracts the request.
+    It does so by looking at the triples contained in the WHERE clause.
+    Only a single request is suported
+  
+    Consider the following SPARQL query:
+
+        PREFIX  clip: <https://service.swissartresearch.net/clip/>
+        SELECT ?iiif ?score WHERE { 
+            ?request a clip:Request ;
+                clip:queryString "A mountain lake" ;
+                clip:minScore "0.2" ;
+                clip:score ?score ;
+                clip:iiifUrl ?iiif .
+        } LIMIT 10
+    """
+    p = parser()
+    try:
+        parsedQuery = p.parseQuery(query)
+    except Exception as e:
+        return {'error': str(e)}
+    
+    def addSelect(request, key, map):
+        if not 'select' in request:
+            request['select'] = {}
+        request['select'][key] = map
+        return request
+
+    def addOption(request, option, value):
+        if not 'options' in request:
+            request['options'] = {}
+        request['options'][option] = value
+        return request
+
+    def getValueWithoutPrefix(value):
+        prefix = 'https://service.swissartresearch.net/clip/'
+        return value[len(prefix):]
+
+    request = {}
+    
+    app.logger.info(parsedQuery)
+
+    for triple in parsedQuery['where']:
+        if triple['s']['type'] == Variable and getValueWithoutPrefix(triple['o']['value']) == 'Request':
+            request['variable'] = triple['s']['value']
+            # Process only first request encountered
+            break
+
+    for triple in parsedQuery['where']:
+        if triple['s']['value'] == request['variable']:
+            if getValueWithoutPrefix(triple['p']['value']) == 'queryString' and triple['o']['type'] == Literal:
+                request['queryString'] = triple['o']['value']
+            elif getValueWithoutPrefix(triple['p']['value']) == 'minScore' and triple['o']['type'] == Literal:
+                request = addOption(request, 'minScore', float(triple['o']['value']))
+            elif getValueWithoutPrefix(triple['p']['value']) == 'iiifUrl' and triple['o']['type'] == Variable:
+                request = addSelect(request, 'url', triple['o']['value'])
+            elif getValueWithoutPrefix(triple['p']['value']) == 'score' and triple['o']['type'] == Variable:
+                request = addSelect(request, 'score', triple['o']['value'])
+
+    if 'limitOffset' in parsedQuery:
+        if 'limit' in parsedQuery['limitOffset']:
+            request = addOption(request, 'numResults', int(parsedQuery['limitOffset']['limit']))
+
+    return request
+
+def processSparqlQuery(query):
+  """
+  Accepts a parsed SPARQL query, extracts and processes the requests and returns a SPARQL response.
+  """
+  request = extractRequestFromSparqlQuery(query)
+  #result = queryWithRequest(query)
+  return request
+
 def queryWithString(queryString, *, minScore=0.2, numResults=10):
     results = clipQuery.query(queryString, numResults=numResults, minScore=minScore)
     for result in results:
