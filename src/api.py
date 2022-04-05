db4e64fd491b830ab32884933d927f702a248a09
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-04-05 17:17:05 +0200

    add support for limit and minscore

--- src/api.py
+++ src/api.py
@@ -24,12 +24,20 @@ def index():
 def query():
     if 'str' in request.values:
         queryString = request.values['str']
-        result = queryWithString(queryString)
+        if 'limit' in request.values:
+            limit = int(request.values['limit'])
+        else:
+            limit = 10
+        if 'minScore' in request.values:
+            minScore = float(request.values['minScore'])
+        else:
+            minScore = 0.2
+        result = queryWithString(queryString, minScore=minScore, numResults=limit)
         return Response(json.dumps(result), mimetype='application/json')
     return Response('{"status": "OK"}', mimetype='application/json')
     
-def queryWithString(queryString):
-    results = clipQuery.query(queryString, numResults=3)
+def queryWithString(queryString, *, minScore=0.2, numResults=10):
+    results = clipQuery.query(queryString, numResults=numResults, minScore=minScore)
     for result in results:
         result['link'] = result['url'] + '/full/1000,/0/default.jpg'
     return results
